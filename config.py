24be3f7eafabbff7ce8326a2944b3f16aaa175f0
Andrew <chistyakgg@gmail.com>
2026-05-01 18:02:54 +0300

    new sources

--- config.py
+++ config.py
@@ -3,6 +3,13 @@ FEEDS = [
     "https://cointelegraph.com/rss",
     "https://rss.nytimes.com/services/xml/rss/nyt/Technology.xml",
     "https://feeds.feedburner.com/TechCrunch/",
+    "https://www.rbc.ru/rss/rbcnews.rss",
+    "https://www.kommersant.ru/RSS/news.xml",
+    "https://www.forbes.ru/newrss.xml",
+    "https://www.rc.ru/rss",
+    "https://habr.com/ru/rss/hub/finances/",
+    "https://forklog.com/feed"
+
 ]
 
 KEYWORDS = [
